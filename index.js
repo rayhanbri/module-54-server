3067ff02ae97581624d0df7473bd3848819ec5d2
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-15 14:12:26 -0700

    data recive

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 
-app.use(cors());
+app.use(cors()); 
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('user server isszz running')
 })
@@ -25,6 +26,15 @@ const users = [
 
 app.get('/users', (req,res) => {
     res.send(users);
+})
+
+
+app.post ('/users',(req,res)=>{
+    console.log('user is sending data')
+    console.log(req.body)
+    const newUser = req.body
+    newUser.id  = users.length + 1;
+    res.send(newUser)
 })
 
 
