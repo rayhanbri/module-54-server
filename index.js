555d857edd459cccdc1c807a9504d2012a009d46
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-15 09:46:57 -0700

    nodemon and watch added

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.get('/', (req, res) => {
 })
 
 
+// npm i -g nodemon
+
+const users = [
+    {id:1,name:'Sabana',email:'sabana@gmail.com'},
+    {id:2,name:'Sabnoor',email:'sabnoor@gmail.com'},
+    {id:3,name:'Sabilanoor',email:'sabilanoor@gmail.com'},
+]
+
+app.get('/users', (req,res) => {
+    res.send(users);
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
