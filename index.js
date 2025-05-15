1fb0427d25de0e9e18ee4973497dc1f1a4913173
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-15 09:18:19 -0700

    node esxpress added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('user server isszz running')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
