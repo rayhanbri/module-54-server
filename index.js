4a2ef04655dc7cb1ef041e7c2ba2c1d2f6482c97
rayhanbri <mohammadrayhanx0@gmail.com>
2025-05-15 10:09:44 -0700

    cors and express installed and cors used

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('user server isszz running')
 })
 
 
 // npm i -g nodemon
+
+//node --watch index.js
+
+// ei duitar je khono ekta open korte hobe shob somoi vs code er terminal
+// theke eishob kajkarbar korte hobe 
 
 const users = [
     {id:1,name:'Sabana',email:'sabana@gmail.com'},
